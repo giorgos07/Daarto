6b053b10845dcc7e01c4a79351a3b6147bb6fde1
Georgios Manoltzas <g.manoltzas@indice.gr>
2018-09-13 18:46:08 +0300

    remove bower and use npm along with a gulp tasks for libs, change structure of project folders

---
+++ src/Daarto.WebUI/gulpfile.js
@@ -0,0 +1,22 @@
+﻿'use strict';
+
+var gulp = require('gulp'),
+    del = require('del'),
+    npmDist = require('gulp-npm-dist');
+
+var lib = './wwwroot/lib/';
+
+gulp.task('clean:lib', function (callback) {
+    del([
+        lib + '**'
+    ]).then(function () {
+        callback();
+    });
+});
+
+gulp.task('copy:libs', function (callback) {
+    gulp.src(npmDist(), { base: './node_modules' })
+        .pipe(gulp.dest(lib));
+
+    callback();
+});
